01ed436b3c523b82cc1d670bb3de2a681cc3ee1c
Joaoguilhermelima <joao.ramos@dcomp.ufs.br>
2026-06-24 15:11:06 -0300

    Oval e retângulo
    Adicionadas funcionalidades de ovais e retângulos. Além disso, agora as figuras ficam na tela ao fazer outra figura.

--- codigo.py
+++ codigo.py
@@ -1,29 +1,59 @@
 import tkinter as tk
 
+def seleciona_linha():
+    global ferramenta
+    ferramenta = "linha"
+
+def seleciona_retangulo():
+    global ferramenta
+    ferramenta = "retangulo"
+
+def seleciona_oval():
+    global ferramenta
+    ferramenta = "oval"
+
 def marca_inicio(event):
     global ini_x, ini_y
     ini_x = event.x
     ini_y = event.y
 
-def atualiza_fim(event):
-    global fim_x, fim_y
-    fim_x = event.x
-    fim_y = event.y
-    canvas.delete("all")
-    canvas.create_line(ini_x, ini_y, fim_x, fim_y)
+def atualiza_figura(event):
+    global figura_temp
+    canvas.delete("preview")
+    
+    if ferramenta == "linha":
+        canvas.create_line(ini_x, ini_y, event.x, event.y, tags = "preview")
+    elif ferramenta == "retangulo":
+        canvas.create_rectangle(ini_x, ini_y, event.x, event.y, tags = "preview")
+    elif ferramenta == "oval":
+        canvas.create_oval(ini_x, ini_y, event.x, event.y, tags = "preview")
 
+def atualiza_fim(event):
+   canvas.delete("preview")
+
+   if ferramenta == "linha":
+       canvas.create_line(ini_x, ini_y, event.x, event.y)
+   elif ferramenta == "retangulo":
+       canvas.create_rectangle(ini_x, ini_y, event.x, event.y)
+   elif ferramenta == "oval":
+       canvas.create_oval(ini_x, ini_y, event.x, event.y)
 
 root = tk.Tk()
+
+ferramenta = "linha"
+
+frame = tk.Frame(root)
+frame.pack()
+
+tk.Button(frame, text = "Linha", command = seleciona_linha).pack(side = tk.LEFT)
+tk.Button(frame, text = "Retângulo", command = seleciona_retangulo).pack(side = tk.LEFT)
+tk.Button(frame, text = "Oval", command = seleciona_oval).pack(side = tk.LEFT)
 
 canvas = tk.Canvas(root, bg='white', width=600, height=600)
 canvas.pack()
 
-ini_x = None  # coordenadas do ponto inicial da linha
-ini_y = None
-fim_x = None
-fim_y = None
-canvas.bind('<ButtonPress-1>', marca_inicio)
-canvas.bind('<B1-Motion>', atualiza_fim)
-#canvas.bind('<ButtonRelease-1>', reset)
+canvas.bind("<ButtonPress-1>", marca_inicio)
+canvas.bind("<B1-Motion>", atualiza_figura)
+canvas.bind("<ButtonRelease-1>", atualiza_fim)
 
 root.mainloop()
