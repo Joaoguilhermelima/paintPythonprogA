e49f3d0daf07bbcefb6cffd3acea0bb0c672d17e
Joaoguilhermelima <joao.ramos@dcomp.ufs.br>
2026-06-25 14:04:31 -0300

    Criar figura

---
+++ Figura.py
@@ -0,0 +1,11 @@
+class Figura:
+    def __init__(self, x1, y1, x2, y2, cor_borda = "black", cor_preenchimento = ""):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.cor_borda = cor_borda
+        self.cor_preenchimento = cor_preenchimento
+
+    def desenhar(self, canvas):
+        raise NotImplementedError
