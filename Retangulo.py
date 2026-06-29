30c80939be17a792f405df0dde57343f2dcaa90d
ricardoherma <ricardohermanxa@gmail.com>
2026-06-29 02:15:57 -0300

    Update Retangulo.py

--- Retangulo.py
+++ Retangulo.py
@@ -1,3 +1,5 @@
+from Figuras import Figura
+
 class Retangulo(Figura):
     def desenhar(self, canvas):
         return canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, outline = self.cor_borda, fill = self.cor_preenchimento)
