b0fdd2cb2067a116d5c171ceab7d983520890ed8
Joaoguilhermelima <joao.ramos@dcomp.ufs.br>
2026-07-02 14:22:47 -0300

    Mão livre

--- codigo.py
+++ codigo.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 from tkinter import colorchooser
+from MaoLivre import MaoLivre
+
+def seleciona_maolivre():
+    global ferramenta
+    ferramenta = "maolivre"
 
 def seleciona_linha():
     global ferramenta
@@ -26,12 +31,22 @@ def escolhe_cor_preenchimento():
         cor_preenchimento = cor
 
 def marca_inicio(event):
-    global ini_x, ini_y
+    global ini_x, ini_y, mao_livre
     ini_x = event.x
     ini_y = event.y
 
+    if ferramenta == "maolivre":
+        mao_livre = MaoLivre(event.x, event.y, cor_borda)
+
 def atualiza_figura(event):
-    global figura_temp
+    global figura_temp, mao_livre
+
+    if ferramenta == "maolivre":
+        canvas.delete("preview")
+        mao_livre.adicionar_ponto(event.x, event.y)
+        mao_livre.desenhar(canvas, tags="preview")
+        return
+    
     canvas.delete("preview")
     
     if ferramenta == "linha":
@@ -42,7 +57,9 @@ def atualiza_figura(event):
         canvas.create_oval(ini_x, ini_y, event.x, event.y, outline = cor_borda, fill = cor_preenchimento, tags = "preview")
 
 def atualiza_fim(event):
+   global mao_livre
    canvas.delete("preview")
+
 
    if ferramenta == "linha":
        canvas.create_line(ini_x, ini_y, event.x, event.y, fill = cor_borda)
@@ -50,12 +67,17 @@ def atualiza_fim(event):
        canvas.create_rectangle(ini_x, ini_y, event.x, event.y, outline = cor_borda, fill = cor_preenchimento)
    elif ferramenta == "oval":
        canvas.create_oval(ini_x, ini_y, event.x, event.y, outline = cor_borda, fill = cor_preenchimento)
+   elif ferramenta == "maolivre":
+       mao_livre.adicionar_ponto(event.x, event.y)
+       mao_livre.desenhar(canvas)
+       mao_livre = None
 
 root = tk.Tk()
 
 ferramenta = "linha"
 cor_borda = "black"
 cor_preenchimento = ""
+mao_livre = None
 
 frame = tk.Frame(root)
 frame.pack()
@@ -63,6 +85,7 @@ frame.pack()
 tk.Button(frame, text = "Linha", command = seleciona_linha).pack(side = tk.LEFT)
 tk.Button(frame, text = "Retângulo", command = seleciona_retangulo).pack(side = tk.LEFT)
 tk.Button(frame, text = "Oval", command = seleciona_oval).pack(side = tk.LEFT)
+tk.Button(frame, text="Mão Livre", command=seleciona_maolivre).pack(side=tk.LEFT)
 tk.Button(frame, text = "Cor da borda", command = escolhe_cor_borda).pack(side = tk.LEFT)
 tk.Button(frame, text = "Cor de preenchimento", command = escolhe_cor_preenchimento).pack(side = tk.LEFT)
 
