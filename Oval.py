4a138bdd215eb5913234d43b0a2c948eda835894
Joaoguilhermelima <joao.ramos@dcomp.ufs.br>
2026-07-07 15:15:12 -0300

    Delete Oval.py

--- Oval.py
+++
@@ -1,5 +0,0 @@
-from Figuras import Figura
-
-class Oval(Figura):
-    def desenhar(self, canvas):
-        return canvas.create_oval(self.x1, self.y1, self.x2, self.y2, outline = self.cor_borda, fill = self.cor_preenchimento)
