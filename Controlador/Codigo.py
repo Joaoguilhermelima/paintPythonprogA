4219507e38646c62d0087d59c218d5aea7329d19
Joaoguilhermelima <joao.ramos@dcomp.ufs.br>
2026-07-07 15:26:00 -0300

    Create Codigo.py

---
+++ Controlador/Codigo.py
@@ -0,0 +1,78 @@
+import tkinter as tk
+from tkinter import colorchooser
+from MaoLivre import MaoLivre
+
+def seleciona_maolivre():
+    global ferramenta
+    ferramenta = "maolivre"
+
+def seleciona_linha():
+    global ferramenta
+    ferramenta = "linha"
+
+def seleciona_retangulo():
+    global ferramenta
+    ferramenta = "retangulo"
+
+def seleciona_oval():
+    global ferramenta
+    ferramenta = "oval"
+
+def escolhe_cor_borda():
+    global cor_borda
+    cor = colorchooser.askcolor(title = "Escolha a cor da borda")[1]
+    if cor:  
+        cor_borda = cor
+
+def escolhe_cor_preenchimento():
+    global cor_preenchimento
+    cor = colorchooser.askcolor(title = "Escolha a cor de preenchimento")[1]
+    if cor:  
+        cor_preenchimento = cor
+
+def marca_inicio(event):
+    global ini_x, ini_y, mao_livre
+    ini_x = event.x
+    ini_y = event.y
+
+    if ferramenta == "maolivre":
+        mao_livre = MaoLivre(event.x, event.y, cor_borda)
+
+def atualiza_figura(event):
+    global figura_temp, mao_livre
+
+    if ferramenta == "maolivre":
+        canvas.delete("preview")
+        mao_livre.adicionar_ponto(event.x, event.y)
+        mao_livre.desenhar(canvas, tags="preview")
+        return
+    
+    canvas.delete("preview")
+    
+    if ferramenta == "linha":
+        canvas.create_line(ini_x, ini_y, event.x, event.y, fill = cor_borda, tags = "preview")
+    elif ferramenta == "retangulo":
+        canvas.create_rectangle(ini_x, ini_y, event.x, event.y, outline = cor_borda, fill = cor_preenchimento, tags = "preview")
+    elif ferramenta == "oval":
+        canvas.create_oval(ini_x, ini_y, event.x, event.y, outline = cor_borda, fill = cor_preenchimento, tags = "preview")
+
+def atualiza_fim(event):
+   global mao_livre
+   canvas.delete("preview")
+
+
+   if ferramenta == "linha":
+       canvas.create_line(ini_x, ini_y, event.x, event.y, fill = cor_borda)
+   elif ferramenta == "retangulo":
+       canvas.create_rectangle(ini_x, ini_y, event.x, event.y, outline = cor_borda, fill = cor_preenchimento)
+   elif ferramenta == "oval":
+       canvas.create_oval(ini_x, ini_y, event.x, event.y, outline = cor_borda, fill = cor_preenchimento)
+   elif ferramenta == "maolivre":
+       mao_livre.adicionar_ponto(event.x, event.y)
+       mao_livre.desenhar(canvas)
+       mao_livre = None
+
+ferramenta = "linha"
+cor_borda = "black"
+cor_preenchimento = ""
+mao_livre = None
