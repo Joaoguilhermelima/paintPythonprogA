e3b42323089868b422e464087609d2d032e9358f
ricardoherma <ricardohermanxa@gmail.com>
2026-07-13 21:34:37 -0300

    Update Linha.py

--- Modelo/Linha.py
+++ Modelo/Linha.py
@@ -1,5 +1,17 @@
-from Figuras import Figura
+from Modelo.Figuras import Figura
 
 class Linha(Figura):
     def desenhar(self, canvas):
-        return canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill = self.cor_borda)
+        return canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill=self.cor_borda)
+
+    def to_dict(self):
+        return {
+            "tipo": "Linha",
+            "x1": self.x1, "y1": self.y1,
+            "x2": self.x2, "y2": self.y2,
+            "cor_borda": self.cor_borda
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return Linha(data["x1"], data["y1"], data["x2"], data["y2"], cor_borda=data["cor_borda"])
