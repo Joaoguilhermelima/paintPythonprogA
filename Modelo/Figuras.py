9eae081dea3b70236b439e79c78875fa998f8907
ricardoherma <ricardohermanxa@gmail.com>
2026-07-13 21:34:14 -0300

    Update Figuras.py

--- Modelo/Figuras.py
+++ Modelo/Figuras.py
@@ -1,5 +1,5 @@
-class Figura:
-    def __init__(self, x1, y1, x2, y2, cor_borda = "black", cor_preenchimento = ""):
+class Figura(ABC):
+    def __init__(self, x1, y1, x2, y2, cor_borda="black", cor_preenchimento=""):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
@@ -7,5 +7,15 @@ class Figura:
         self.cor_borda = cor_borda
         self.cor_preenchimento = cor_preenchimento
 
+    @abstractmethod
     def desenhar(self, canvas):
+        raise NotImplementedError
+
+    @abstractmethod
+    def to_dict(self):
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def from_dict(data):
         raise NotImplementedError
