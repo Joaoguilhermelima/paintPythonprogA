2473dba9aa28b9dac4f63d737ad52603cdd3aa85
ricardoherma <ricardohermanxa@gmail.com>
2026-07-14 14:23:39 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,12 @@
 import tkinter as tk
 from tkinter import colorchooser
 
-from Controlador.ControladorDesenho import ControladorDesenho 
-from Controlador.Estados import EstadoLinha, EstadoRetangulo, EstadoOval, EstadoMaoLivre
 
+from Controlador.Codigo import ControladorDesenho 
+from Controlador.Estados import EstadoLinha, EstadoRetangulo, EstadoOval, EstadoMaoLivre
 
 root = tk.Tk()
 root.title("Editor de Desenho")
-
 
 canvas = tk.Canvas(root, bg='white', width=600, height=600)
 canvas.pack()
@@ -37,23 +36,20 @@ def mudar_para_maolivre():
 frame = tk.Frame(root)
 frame.pack()
 
-
 tk.Button(frame, text="Linha", command=mudar_para_linha).pack(side=tk.LEFT)
 tk.Button(frame, text="Retângulo", command=mudar_para_retangulo).pack(side=tk.LEFT)
 tk.Button(frame, text="Oval", command=mudar_para_oval).pack(side=tk.LEFT)
 tk.Button(frame, text="Mão Livre", command=mudar_para_maolivre).pack(side=tk.LEFT)
 
-
 tk.Button(frame, text="Cor Borda", command=controlador.escolhe_cor_borda).pack(side=tk.LEFT)
 tk.Button(frame, text="Cor Preenchimento", command=controlador.escolhe_cor_preenchimento).pack(side=tk.LEFT)
-
 
 tk.Button(frame, text="Salvar", command=controlador.salvar).pack(side=tk.LEFT)
 tk.Button(frame, text="Abrir", command=controlador.abrir).pack(side=tk.LEFT)
 
 
-canvas.bind("<ButtonPress-1>", lambda event: controlador.estado_atual.pressionar(event, controlador))
-canvas.bind("<B1-Motion>", lambda event: controlador.estado_atual.arrastar(event, controlador))
-canvas.bind("<ButtonRelease-1>", lambda event: controlador.estado_atual.soltar(event, controlador))
+canvas.bind("<ButtonPress-1>", lambda event: controlador.estado_atual.pressionar(controlador, event))
+canvas.bind("<B1-Motion>", lambda event: controlador.estado_atual.arrastar(controlador, event))
+canvas.bind("<ButtonRelease-1>", lambda event: controlador.estado_atual.soltar(controlador, event))
 
 root.mainloop()
