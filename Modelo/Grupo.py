52237fe968f6bfd5bec058945b818b8331c48bec
ricardoherma <ricardohermanxa@gmail.com>
2026-07-16 13:47:30 -0300

    Update Grupo.py

--- Modelo/Grupo.py
+++ Modelo/Grupo.py
@@ -6,7 +6,7 @@ class Grupo(Figura):
         self.figuras = figuras if figuras is not None else []
         self.recalcular_limites()
 
-    def recalcular_limites(self):
+    def recalcular_limites(self):             #Calcula a área mínima necessária para englobar todas as figuras do grupo
         if not self.figuras:
             self.x1, self.y1, self.x2, self.y2 = 0, 0, 0, 0
             return
@@ -23,11 +23,11 @@ class Grupo(Figura):
         self.x2 = max(x2_list)
         self.y2 = max(y2_list)
 
-    def desenhar(self, canvas):
+    def desenhar(self, canvas):    # Percorre a lista de figuras internas e desenha cada uma delas na tela
         for fig in self.figuras:
             fig.desenhar(canvas)
 
-    def mover(self, dx, dy):
+    def mover(self, dx, dy):     # Desloca todas as figuras internas e a própria caixa limite do grupo pelas distâncias especificadas
         for fig in self.figuras:
             fig.mover(dx, dy)
         self.x1 += dx
@@ -35,14 +35,14 @@ class Grupo(Figura):
         self.x2 += dx
         self.y2 += dy
 
-    def contem(self, x, y):
+    def contem(self, x, y):   # Desloca todas as figuras internas e a própria caixa limite do grupo pelas distâncias especificadas
         for fig in self.figuras:
             if fig.contem(x, y):
                 return True
         return False
 
     @property
-    def cor_borda(self):
+    def cor_borda(self):     # Altera a cor de contorno do grupo e propaga essa nova cor para todas as figuras internas
         return self._cor_borda
 
     @cor_borda.setter
@@ -52,7 +52,7 @@ class Grupo(Figura):
             fig.cor_borda = cor
 
     @property
-    def cor_preenchimento(self):
+    def cor_preenchimento(self):   # Altera a cor de preenchimento do grupo e propaga essa nova cor para todas as figuras internas
         return self._cor_preenchimento
 
     @cor_preenchimento.setter
@@ -61,8 +61,8 @@ class Grupo(Figura):
         for fig in self.figuras:
             fig.cor_preenchimento = cor
 
-    def to_dict(self):
-        return {
+    def to_dict(self):             # Transforma o grupo e suas subfiguras em um dicionário para permitir que o desenho seja salvo em arquivo
+        return { 
             "tipo": "Grupo",
             "figuras": [fig.to_dict() for fig in self.figuras],
             "cor_borda": self.cor_borda,
@@ -70,7 +70,7 @@ class Grupo(Figura):
         }
 
     @staticmethod
-    def from_dict(data):
+    def from_dict(data):          # Lê o dicionário salvo, identifica os tipos de figuras e reconstrói o objeto Grupo original
         from Modelo.Linha import Linha
         from Modelo.Retangulo import Retangulo
         from Modelo.Oval import Oval
